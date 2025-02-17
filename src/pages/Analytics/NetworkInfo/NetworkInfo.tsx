34681b4a5587b2330d3cd2abf06c4f390d1b098d
Vinay Pallegar <vinay.pallegar@movementlabs.xyz>
2025-02-17 11:18:11 -0800

    fix: enable mn analytics - hide tps for now.

--- src/pages/Analytics/NetworkInfo/NetworkInfo.tsx
+++ src/pages/Analytics/NetworkInfo/NetworkInfo.tsx
@@ -62,11 +62,11 @@ export default function NetworkInfo({isOnHomePage}: NetworkInfoProps) {
             <TotalTransactions type={"card"} />
           </LinkableContainer>
         </Grid>
-        <Grid item xs={12} md={3} lg={2.4}>
+        {/* <Grid item xs={12} md={3} lg={2.4}>
           <LinkableContainer linkToAnalyticsPage={onHomePage}>
             <TPS />
           </LinkableContainer>
-        </Grid>
+        </Grid> */}
         <Grid item xs={12} md={3} lg={2.4}>
           <LinkableContainer linkToAnalyticsPage={onHomePage}>
             <TotalNewAccountsCreated
